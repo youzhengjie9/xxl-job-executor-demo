617df65763a2ba3b26ecbd0c829169757817a2fa
ms666 <1550324080@qq.com>
2022-08-22 00:41:33 +0800

    first commit

---
+++ src/main/java/com/boot/XxlJobExecutorApplication.java
@@ -0,0 +1,17 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author youzhengjie
+ * @since 2022-08-22 00:16:21
+ */
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class,args);
+    }
+
+}
